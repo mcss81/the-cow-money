d111adb5dcbb73cfdfad42779588e7f08e7d10f6
mcss81 <mariosanchezsaltos@gmail.com>
2026-09-24 16:44:54 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-window.SUPABASE_URL = "PEGA_AQUI_TU_SUPABASE_URL";
-window.SUPABASE_ANON_KEY = "PEGA_AQUI_TU_SUPABASE_ANON_KEY";
+window.SUPABASE_URL = "https://evjvxcdcvcnqofngfbir.supabase.co";
+window.SUPABASE_ANON_KEY = "sb_publishable_gw9bXbdpAsglIsOZDfkw9A_fAWnsh2Y";
